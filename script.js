b82ca6343e84e0ff67eff87b8af27d9c0f842fb2
SoraY677 <SoraY677@outlook.com>
2020-03-01 14:45:20 +0900

    会話表示までできた

--- script.js
+++ script.js
@@ -3,28 +3,57 @@
  */
 //GooclechromeとFirefoxに対応
 SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
-  const recognition = new SpeechRecognition();
+const recognition = new SpeechRecognition();
 
-	//言語指定：日本語
-	recognition.lang = 'ja-JP';
-	//認識途中のデータ取得
-	recognition.interimResults = true;
-	//認識し続ける
-	recognition.continuous = true;
+//言語指定：日本語
+recognition.lang = "ja-JP";
+//認識途中のデータ取得
+recognition.interimResults = true;
+//認識し続ける
+recognition.continuous = true;
 
-
-	let finalTranscript = "";
-  recognition.onresult = (event) => {
-		let interimTranscript = "";
-		for (let eventi = event.resultIndex; eventi < event.results.length; eventi++) {
-			let transcript = event.results[eventi][0].transcript;
-			if (event.results[eventi].isFinal) {
-        finalTranscript += transcript;
-      } else {
-        interimTranscript = transcript;
-      }
+let finalTranscript = "";
+recognition.onresult = event => {
+	let interimTranscript = "";
+	for (
+		let eventi = event.resultIndex;
+		eventi < event.results.length;
+		eventi++
+	) {
+		let transcript = "<p class=\"talk-section\">" + event.results[eventi][0].transcript + "</p>";
+		if (event.results[eventi].isFinal) {
+			finalTranscript += transcript;
+		} else {
+			interimTranscript = transcript;
 		}
-		document.getElementById("rec_text").innerHTML = finalTranscript + interimTranscript;
-  }
+	}
+	document.getElementById("rec_text").innerHTML =
+		finalTranscript + interimTranscript;
+};
 
-  recognition.start();
+//=====================================
+//以下、DOM系統
+//=====================================
+
+//cssの".active"を付与する場合
+const activeClass = "active";
+
+/**
+ * RECボタンの設定
+ */
+let recBt = document.getElementById("rec_bt");
+
+recBt.addEventListener("click", function() {
+	//押されていなかった場合(デフォルト)
+	if (!recBt.classList.contains(activeClass)) {
+		//録音開始
+		recBt.classList.add(activeClass);
+		recognition.start();
+	}
+	//押されていた場合
+	else {
+		//録音停止
+		recBt.classList.remove(activeClass);
+		recognition.stop();
+	}
+});
